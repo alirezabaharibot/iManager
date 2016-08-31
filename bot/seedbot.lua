f69208f674fe6b031035bdef4df0b813dc016bfa
Mobin Dehghani <mobindehghani.md@gmail.com>
2016-08-31 12:30:48 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -228,12 +228,13 @@ function create_config( )
     "invite",
     "all",
     "rmsg",
+    "lock_fosh",
     "leave_ban",
 	"supergroup",
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {160149610,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
