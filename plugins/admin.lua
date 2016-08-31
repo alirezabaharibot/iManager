ee14a8731f3d0aecd7239112c0458bcf9b900b78
Mobin Dehghani <mobindehghani.md@gmail.com>
2016-08-31 10:44:52 -0700

    Reloaded :)

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -261,8 +261,8 @@ end
 	if matches[1] == 'reload' then
 		receiver = get_receiver(msg)
 		reload_plugins(true)
-		post_msg(receiver, "Reloaded!", ok_cb, false)
-		return "Reloaded!"
+		post_msg(receiver, "🔄 روبات با موفقیت ریلود شد", ok_cb, false)
+		return ""
 	end
 	--[[*For Debug*
 	if matches[1] == "vardumpmsg" and is_admin1(msg) then
