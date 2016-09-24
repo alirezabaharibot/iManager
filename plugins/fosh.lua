d31eeda06f35b32620e6d2909f60d7fa95f45c15
Mobin Dehghani <mobindehghani.md@gmail.com>
2016-09-24 03:20:20 -0700

    Create fosh.lua

---
+++ plugins/fosh.lua
@@ -0,0 +1,16 @@
+
+do
+
+function run(msg, matches)
+  return "کـس نـنـت " .. matches[1]
+end
+
+return {
+    patterns = {
+    "^فحش بده به (.*)$",
+    "^[Ff]osh [Bb]ede [Bb]e (.*)$",
+  }, 
+  run = run 
+}
+
+end
